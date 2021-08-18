04cf7fc9f07fad9480625e3a2effaa0f622c8821
Frank Valdes <foyaca86@gmail.com>
2021-08-18 01:25:11 -0400

    forgot refactor...change after submit

--- award.rb
+++ award.rb
@@ -8,5 +8,55 @@ class Award
     @expires_in = expires_in
     @quality = quality
   end
+
+  def self.update_quality(awards)
+    awards.each do |award|
+      if award.name != 'Blue First' && award.name != 'Blue Compare'
+        if award.quality > 0 && award.name != 'Blue Distinction Plus'
+          award.quality -= 1
+          if award.name == 'Blue Star' && award.quality > 0
+            award.quality -= 1
+          end
+        end
+      else
+        if award.quality < 50
+          award.quality += 1
+          if award.name == 'Blue Compare'
+            if award.expires_in < 11
+              if award.quality < 50
+                award.quality += 1
+              end
+            end
+            if award.expires_in < 6
+              if award.quality < 50
+                award.quality += 1
+              end
+            end
+          end
+        end
+      end
+      if award.name != 'Blue Distinction Plus'
+        award.expires_in -= 1
+      end
+      if award.expires_in < 0
+        if award.name != 'Blue First'
+          if award.name != 'Blue Compare'
+            if award.quality > 0 && award.name != 'Blue Distinction Plus'
+              award.quality -= 1
+              if award.name == 'Blue Star' && award.quality > 0
+                award.quality -= 1
+              end
+            end
+          else
+            award.quality = award.quality - award.quality
+          end
+        else
+          if award.quality < 50
+            award.quality += 1
+          end
+        end
+      end
+    end
+  end 
   
 end
